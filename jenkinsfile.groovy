fe0792aa44dc6fcbf4e5998ccc56e82aa7dcdce5
tosinade314 <tadebote@gmail.com>
2023-11-20 23:04:34 -0600

    fix

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -5,15 +5,21 @@ pipeline {
         stage('Checkout') {
             steps {
                 // Checkout code from the repository
-                git branch: 'main', url: 'https://github.com/reniade314/flask-project.git'
+                git branch: 'main', credentialsId: '3e7c1305-8065-4201-b132-d794720a01e1', url: 'https://github.com/reniade314/flask-project.git'
             }
         }
 
         stage('Deploy') {
             steps {
                 // Execute Ansible playbook for deployment
-                ansiblePlaybook credentialsId: 'flask-1', disableHostKeyChecking: true, installation: 'Ansible', inventory: '/home/centos/flask-project/node.ini', playbook: '/home/centos/flask-project/01-install-flask.yml', vaultTmpPath: ''
-                '''
+                ansiblePlaybook(
+                    credentialsId: 'flask-1',
+                    disableHostKeyChecking: true,
+                    installation: 'Ansible',
+                    inventory: '/home/centos/flask-project/node.ini',
+                    playbook: '/home/centos/flask-project/01-install-flask.yml',
+                    vaultTmpPath: ''
+                )
             }
         }
     }
