01d03f52007a9320ba10e5b9cc75f8241fff46ca
tosinade314 <tadebote@gmail.com>
2023-11-20 19:04:16 -0600

    fix

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "I am almost a Devops Engineer!"
+    return "I am almost a DevOps Engineer!"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)  # Change the port number here
