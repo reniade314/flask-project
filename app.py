4c5f89b39ce23cad32133c8caf39814bff6c61b1
reniade314 <144544849+reniade314@users.noreply.github.com>
2023-11-27 18:54:52 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "I am almost a DevOps Engineer!"
+    return "I am almost a DevOps Engineer and SRE!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)  # Change the port number here
